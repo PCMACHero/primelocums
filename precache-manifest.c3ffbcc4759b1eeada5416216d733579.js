3235a1107c2aabea5ce3778de5749e59fae2eab8
Oang <oang@Oangs-MacBook-Pro.local>
2019-03-13 19:44:54 -0700

    Updates

---
+++ precache-manifest.c3ffbcc4759b1eeada5416216d733579.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "22331012a3f15856d3b8ef54c2313187",
+    "url": "/primelocums/static/media/Twitter.22331012.svg"
+  },
+  {
+    "revision": "042ff1623b870f6800ca",
+    "url": "/primelocums/static/css/main.a7b16a1e.chunk.css"
+  },
+  {
+    "revision": "778cae2af8c525819091",
+    "url": "/primelocums/static/js/runtime~main.778cae2a.js"
+  },
+  {
+    "revision": "5bcaf6cffcedef3247569a9f8016a7bf",
+    "url": "/primelocums/static/media/e-pick.5bcaf6cf.png"
+  },
+  {
+    "revision": "89f1fbf31e1e12b80da9",
+    "url": "/primelocums/static/js/2.89f1fbf3.chunk.js"
+  },
+  {
+    "revision": "b6665081c29f104c400391d7814befa1",
+    "url": "/primelocums/static/media/hp-bg9.b6665081.jpeg"
+  },
+  {
+    "revision": "8fc7a048c95e3759caeaa7e5d0661c0a",
+    "url": "/primelocums/static/media/LinkedIn.8fc7a048.svg"
+  },
+  {
+    "revision": "042ff1623b870f6800ca",
+    "url": "/primelocums/static/js/main.042ff162.chunk.js"
+  },
+  {
+    "revision": "f8f76b60ed73f091a6b856cdb1346a91",
+    "url": "/primelocums/static/media/Facebook.f8f76b60.svg"
+  },
+  {
+    "revision": "4bc804254831288c1f2c52797c996342",
+    "url": "/primelocums/static/media/cl-bg.4bc80425.jpeg"
+  },
+  {
+    "revision": "79fc0c8cb06e46f724c677f68ffa4449",
+    "url": "/primelocums/static/media/hp-bg6.79fc0c8c.jpeg"
+  },
+  {
+    "revision": "3cfd5bb42bd093b3771d84bd1dda0144",
+    "url": "/primelocums/static/media/hp-bg8.3cfd5bb4.jpeg"
+  },
+  {
+    "revision": "d79bba4b6f67cfa9acaebda9e643a910",
+    "url": "/primelocums/static/media/hp-bg5.d79bba4b.jpeg"
+  },
+  {
+    "revision": "89f1fbf31e1e12b80da9",
+    "url": "/primelocums/static/css/2.a2544b30.chunk.css"
+  },
+  {
+    "revision": "60015b48dcd75077117a5617c6e20894",
+    "url": "/primelocums/index.html"
+  }
+];
