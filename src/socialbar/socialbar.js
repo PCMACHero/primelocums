09081fc664231b4c57169e73ed8a6c98b34a8fc1
pcmachero <pcmacmen@gmail.com>
2019-08-29 22:15:49 -0700

    fixed links, removed watermarked image, added favicons, changed title

--- src/socialbar/socialbar.js
+++ src/socialbar/socialbar.js
@@ -4,9 +4,10 @@ import '../socialbar/socialbar.css'
 const SocialBar = ()=>{
     return (
         <div className="social-bar">
-            <div className="icon twitter"></div>
-            <div className="icon facebook"></div>
-            <div className="icon in"></div>
+            <a href="https://www.linkedin.com/company/prime-locums/" target="_blank"><div className="icon in"></div></a>
+            <a href="https://www.facebook.com/primelocums/?modal=composer&notif_id=1566823765697592&notif_t=aymt_upsell_tip" target="_blank"><div className="icon facebook"></div></a>
+            
+            
         </div>
     )
 }
