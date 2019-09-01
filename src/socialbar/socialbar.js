1e8b2b2b265e9d58d74f1db3e67d8ded54253733
pcmachero <pcmacmen@gmail.com>
2019-08-31 17:10:12 -0700

    added twitter link

--- src/socialbar/socialbar.js
+++ src/socialbar/socialbar.js
@@ -5,8 +5,8 @@ const SocialBar = ()=>{
     return (
         <div className="social-bar">
             <a href="https://www.linkedin.com/company/prime-locums/" target="_blank"><div className="icon in"></div></a>
-            <a href="https://www.facebook.com/primelocums/?modal=composer&notif_id=1566823765697592&notif_t=aymt_upsell_tip" target="_blank"><div className="icon facebook"></div></a>
-            
+            <a href="https://www.facebook.com/primelocums/" target="_blank"><div className="icon facebook"></div></a>
+            <a href="https://twitter.com/LocumsPrime" target="_blank"><div className="icon twitter"></div></a>
             
         </div>
     )
